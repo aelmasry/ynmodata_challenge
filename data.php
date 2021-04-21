7f98973240d30d534f5ec62d589523dba131c477
Ali Salem <admin@alisalem.me>
2021-04-21 07:28:39 +0200

    First commit

---
+++ data.php
@@ -0,0 +1,60 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$client = new GuzzleHttp\Client([
+    // Base URI is used with relative requests
+    'base_uri' => 'https://run.mocky.io',
+]);
+
+// fetch data
+$response = $client->request('GET', '/v3/0d6aab31-bb68-4d89-acc5-bc4148a3cff3');
+
+if (200 == $response->getStatusCode()) {
+    $result =  $response->getBody();
+    // $result = json_decode($result, true);
+    // $data = $result['data'];
+} else {
+    return 'No response from endpoint';
+}
+
+$output = '';
+
+if (isset($_REQUEST["query"]))
+{
+    $jsonq = new Nahid\JsonQ\Jsonq($response->getBody());
+
+    $search = $_REQUEST["query"];
+    $result = $jsonq->from('data')
+        ->where('name', '=', $search)
+        ->orWhere('city', '=', $search)
+        ->orWhere('price', '=', $search)
+        ->get();
+
+    $data = json_decode($result, true);
+}
+
+$output .= '
+  <div class="table-responsive">
+   <table class="table table bordered">
+    <tr>
+     <th>ID</th>
+     <th>Name</th>
+     <th>City</th>
+     <th>Price</th>
+    </tr>
+ ';
+
+ foreach($data as $hotel)
+ {
+    $output .= '
+    <tr>
+        <td>' . $hotel["id"] . '</td>
+        <td>' . $hotel["name"] . '</td>
+        <td>' . $hotel["city"] . '</td>
+        <td>' . $hotel["price"] . '</td>
+    </tr>
+    ';
+ }
+
+echo $output;
